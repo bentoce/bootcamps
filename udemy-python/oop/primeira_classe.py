f5707331998dd4b9190af9ba0acd462bf5465164
bentoce <stiforta@gmail.com>
2021-08-05 22:36:19 -0300

    Meus dados objetos

---
+++ udemy-python/oop/primeira_classe.py
@@ -0,0 +1,9 @@
+class Funcionarios :
+    nome = str(input('Digite seu nome ?: '))
+    sobrenome = str(input('Digite seu Sobrenome ? :'))
+
+usuario1 = Funcionarios()
+
+print(usuario1.nome)
+print(usuario1.sobrenome)
+print(str(input('Digite sua da de nascimento ?: ')))
