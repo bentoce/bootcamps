2587e4955854f063840f00803f533c1dfac6efd6
bentoce <stiforta@gmail.com>
2021-08-05 22:47:49 -0300

    Meus dados objetos1

---
+++ udemy-python/oop/ObjetosClasse.py
@@ -0,0 +1,30 @@
+# class 
+class Funcionarios :
+ pass
+
+
+# criar o objeto
+usuarios1 = Funcionarios()
+usuarios2 = Funcionarios()
+
+# criar os parametros de usuario1
+usuarios1.nome = 'Antonio'
+usuarios1.sobrenome = 'Bento'
+usuarios1.data_nasciomento = '22/04/1985'
+
+
+# criar os paramettro usuarios2
+usuarios2.nome = 'Fran'
+usuarios2.sobrenome = 'Souza'
+usuarios2.data_nasciomento = '02/06/1981'
+
+# print 
+print(usuarios1.nome)
+print(usuarios1.sobrenome)
+print(usuarios1.data_nasciomento)
+
+print('==============================')
+
+print(usuarios2.nome)
+print(usuarios2.sobrenome)
+print(usuarios2.data_nasciomento)
