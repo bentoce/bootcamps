e7f02674d4b589639217ccd703df1863bf31e722
bentoce <stiforta@gmail.com>
2021-08-05 22:07:31 -0300

    OOP

---
+++ udemy-python/erros/Introducao_a_dicionarios.py
@@ -0,0 +1,20 @@
+# Erros 
+ # Excelente para testes 
+ # Não realiza o 'stop' no programa
+ # Mensagens customizandos quando encontra um erro 
+
+try:
+ valor = float(input('Digite o valor do seu produto: '))
+ print(type(valor))
+
+
+except ValueError:
+    print('Favor Digite Numero não Letras !') 
+
+jls_extract_var = 'Mais codigo'
+print(jls_extract_var) 
+
+
+
+
+
---
+++ udemy-python/oop/classes.py
@@ -0,0 +1,9 @@
+# Python Object-Oriented Programing
+# Objetos são partes dentro de uma Class (instancias)
+# Classes são ultizados para agrupar dadas e funções, podendo reutilizar 
+# = ===
+# Class: frutas
+# Ojects: Abacate, Banana...
+
+print(type('Antonio '))
+print(('Antonio ').lower())
